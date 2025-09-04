25c9fffbe8d4768e10a0c7be6f78388fdf12c356
emma324 <emma93324@gmail.com>
2025-09-04 19:48:42 +0800

    feat:新增全局header(安卓)

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,20 +1,26 @@
 import { Stack, useRouter } from "expo-router";
+import { COLORS } from "@/src/utils/colors";
 import store, { RootState } from "@/src/store";
 import { Provider, useDispatch, useSelector } from 'react-redux'
 import { useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
 import { fetchUser } from "@/src/store/slices/userSlice";
+import { SafeAreaProvider,SafeAreaView  } from "react-native-safe-area-context";
 export default function RootLayout() {
   
 
   return (
     <Provider store={store}>
-      <Stack
-        screenOptions={{
-          headerShown: false, // 全局標頭
-        }}
-      />
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1,backgroundColor:COLORS.background }} edges={["top", "left", "right"]}>
+          <Stack
+            screenOptions={{
+              headerShown: false, // 全局隱藏標頭
+            }}
+          />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </Provider>
   );
 }
